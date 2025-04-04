67a79cc23ef97be23caeab0a9ecd91f94e89c960
AAABatteryPowered <157712659+AAABatteryPowered@users.noreply.github.com>
2025-04-04 21:59:52 +0100

    save application to redis

--- bot.go
+++ bot.go
@@ -1,15 +1,30 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"strconv"
 	"strings"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/go-redis/redis"
 )
 
 // Bot Token (You need to replace this with your bot's token)
 const token = "MTMyNzU4Mjg0MTkwNjc5NDU1Ng.GsPPqZ.YSbhP_b0wcJ-PHRmOXTl9PNwySJWrOAC2kFmxI"
 const guildid = "1355623019971608706"
+
+var rds *redis.Client
+
+type Application struct {
+	Link   string
+	Author *discordgo.User
+	ID     string
+}
+
+type UserApplicationGroup []Application
+
+var applications map[int]UserApplicationGroup
 
 // This function will be called when the bot is ready
 func ready(s *discordgo.Session, r *discordgo.Ready) {
@@ -21,6 +36,14 @@ func RegisterCommands(s *discordgo.Session) {
 		{
 			Name:        "submit",
 			Description: "Submits your application.",
+			Options: []*discordgo.ApplicationCommandOption{
+				{
+					Name:        "link",
+					Description: "The link to your application",
+					Type:        discordgo.ApplicationCommandOptionString,
+					Required:    true,
+				},
+			},
 		},
 	}
 
@@ -32,19 +55,53 @@ func RegisterCommands(s *discordgo.Session) {
 	}
 }
 
+func submitApplication(interaction *discordgo.InteractionCreate, link string) error {
+	app := Application{
+		Link:   link,
+		Author: interaction.User,
+		ID:     interaction.User.ID,
+	}
+
+	id, err := strconv.Atoi(interaction.User.ID)
+	if err != nil {
+		fmt.Println("Failed to convert UserID string to integer")
+		return err
+	}
+
+	appgroup, exists := applications[id]
+	if !exists {
+		appgroup = make(UserApplicationGroup, 1, 1)
+	}
+	appgroup = append(appgroup, app)
+
+	jsonString, err := json.Marshal(appgroup)
+	if err != nil {
+		fmt.Println("Failed to Marshal")
+		return err
+	}
+
+	err = rds.HSet("applications", interaction.User.ID, jsonString).Err()
+	if err != nil {
+		fmt.Println("could not set data in hash: %v", err)
+		return err
+	}
+	//discordgo.user is a pointer remember that
+	return nil
+}
+
 func CommandsHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	if i.Type == discordgo.InteractionApplicationCommand {
 		switch i.ApplicationCommandData().Name {
 		case "submit":
 
-			message := "12"
+			var message string
 			if len(i.ApplicationCommandData().Options) > 0 {
 				// If the user has passed an argument for echo
 				message = i.ApplicationCommandData().Options[0].StringValue()
 			}
 
-			fmt.Println(message)
 			if strings.Contains(message, "https://www.youtube.com") || strings.Contains(message, "https://www.youtu.be") {
+
 				embed := discordgo.MessageEmbed{
 					Title:       "rizz",
 					Description: "lord",
@@ -74,6 +131,20 @@ func CommandsHandler(s *discordgo.Session, i *discordgo.InteractionCreate) {
 	}
 }
 
+func InitRedis() {
+	rds = redis.NewClient(&redis.Options{
+		Addr:     "localhost:6379",
+		Password: "", // No password set
+		DB:       0,  // Use default DB
+	})
+	_, err := rds.Ping().Result()
+	if err != nil {
+		fmt.Println(err.Error())
+		return
+	}
+	fmt.Println("Redis started successfully!")
+}
+
 func main() {
 	// Create a new Discord session
 	dg, err := discordgo.New("Bot " + token)
@@ -82,13 +153,19 @@ func main() {
 		return
 	}
 
+	InitRedis()
+
 	intents := discordgo.IntentsGuildMessages | discordgo.IntentsMessageContent | discordgo.IntentsGuildMembers
 
 	dg.Identify.Intents = intents
 
 	dg.AddHandler(ready)
-	dg.AddHandler(RegisterCommands)
-	dg.AddHandler(CommandsHandler)
+	dg.AddHandlerOnce(func(s *discordgo.Session, r *discordgo.Ready) {
+		RegisterCommands(s)
+	})
+	dg.AddHandler(func(s *discordgo.Session, i *discordgo.InteractionCreate) {
+		go CommandsHandler(s, i)
+	})
 
 	err = dg.Open()
 	if err != nil {
